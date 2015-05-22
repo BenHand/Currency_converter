021e97a86e862bca37ce4ef35f8e7a1f9d1b3b29
Benjamin Hand <gneeshot@gmail.com>
2015-05-22 12:05:07 -0500

    equals equals tests(2) functioning

--- lib/currency.rb
+++ lib/currency.rb
@@ -1,14 +1,30 @@
 class Currency
   attr_accessor :code, :amount
 
-def initialize(code, amount)
-  @code = code
-  @amount = amount
+  def initialize(code, amount)
+    @code = code
+    @amount = amount
+  end
+
+  # def +(other)
+
+  #   if Currency ==(other)
+  #     total_amount = other.amount.to_i += @amount.to_i
+  #     total_amount
+  #   end
+
+  end
+
+  def ==(other)
+
+    if other.is_a?(Currency) && other.code == @code
+      return true
+    else
+      return false
+    end
+
+  end
+
 end
-
-
-
-end
-
 
 
